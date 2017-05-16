c3da96d0ceddfa731d39ad73d10eb49b7fa7a1af
ktepe <ketepe@gmail.com>
2017-05-16 05:15:47 -0400

    updates

--- P5_Kemal.py
+++ P5_Kemal.py
@@ -7,87 +7,49 @@ import glob
 import time
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
 from lesson_functions import *
 
 debug_prt=1
 
+# how we pickled while obtaining classifier model feature parameters
 '''
-dist_pickle = pickle.load(open("svc_pickle.p", "rb"))
-svc = dist_pickle["svc"]
-X_scaler = dist_pickle["scaler"]
-orient = dist_pickle["orient"]
-pix_per_cell = dist_pickle["pix_per_cell"]
-cell_per_block = dist_pickle["cell_per_block"]
-spatial_size = dist_pickle["spatial_size"]
-hist_bins = dist_pickle["hist_bins"]
+feature_params={'color_space': "RGB", 'orient': orient, 'pix_per_cell': pix_per_cell,
+                'cell_per_block': cell_per_block, 'hog_channel': "ALL", 'spatial_size':spatial_size,
+                'hist_bins': hist_bins, 'spatial_feat':spatial_feat, 'hist_feat': hist_feat,
+                'hog_feat': hog_feat}
 '''
+#
+feature_params= pickle.load(open("feature_params2.p", "rb"))
+color_space=feature_params["color_space"]
+orient = feature_params["orient"]
+pix_per_cell = feature_params["pix_per_cell"]
+cell_per_block = feature_params["cell_per_block"]
+hog_channel=feature_params["hog_channel"]
+spatial_size = feature_params["spatial_size"]
+hist_bins = feature_params["hist_bins"]
+spatial_feat=feature_params["spatial_feat"]
+hist_feat=feature_params["hist_feat"]
+hog_feat=feature_params["hog_feat"]
+#
+#parameters are obtained
 img = mpimg.imread('./sample/bbox-example-image.jpg')
 
-cars=get_image_paths('./vehicles_smallset/cars1_2_3/*.jpeg')
-notcars=get_image_paths('./non-vehicles_smallset/notcars1_2_3/*.jpeg')
-
-
-### TODO: Tweak these parameters and see how the results change.
-colorspace = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-orient = 9
-pix_per_cell = 8
-cell_per_block = 2
-hog_channel = "ALL" # Can be 0, 1, 2, or "ALL"
-
-t=time.time()
-car_features = extract_features(cars, color_space=colorspace, orient=orient,
-                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                        hog_channel=hog_channel)
-notcar_features = extract_features(notcars, color_space=colorspace, orient=orient,
-                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                        hog_channel=hog_channel)
-t2 = time.time()
-print(round(t2-t, 2), 'Seconds to extract HOG features...')
-# Create an array stack of feature vectors
-X = np.vstack((car_features, notcar_features)).astype(np.float64)
-# Fit a per-column scaler
-X_scaler = StandardScaler().fit(X)
-# Apply the scaler to X
-scaled_X = X_scaler.transform(X)
-
-# Define the labels vector
-y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
-
-
-# Split up data into randomized training and test sets
-rand_state = np.random.randint(0, 100)
-X_train, X_test, y_train, y_test = train_test_split(
-    scaled_X, y, test_size=0.2, random_state=rand_state)
-
-print('Using:',orient,'orientations',pix_per_cell,
-    'pixels per cell and', cell_per_block,'cells per block')
-print('Feature vector length:', len(X_train[0]))
-# Use a linear SVC
-svc = LinearSVC()
-# Check the training time for the SVC
-t=time.time()
-svc.fit(X_train, y_train)
-t2 = time.time()
-print(round(t2-t, 2), 'Seconds to train SVC...')
 # Check the score of the SVC
-
+svc2=joblib.load('svc_model2.p')
+X_scaler=joblib.load('X_scaler_model2.p')
 
 ystart = 400
 ystop = 720
 scale = 1.5
 
-xscaler=StandardScaler(copy=True, with_mean=True, with_std=True)
-orient=9
-pix_per_cel=8
-cell_per_block==1
-#spatial_size=(64,64)
-spatial_size=(32,32)
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
 
-hist_bins= 32
-
-
-out_img = find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
+out_img = find_cars(img, ystart, ystop, scale, svc2, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
                     hist_bins)
 
 plt.imsave('./out_sample/p5_out_img.png',out_img)
--- hog_classifier_ket.py
+++ hog_classifier_ket.py
@@ -12,11 +12,6 @@ from sklearn.externals import joblib
 import pickle
 from lesson_functions import get_hog_features, extract_features
 
-
-# Define a function to extract features from a list of images
-# Have this function call bin_spatial() and color_hist()
-
-
 # Divide up into cars and notcars
 debug_prt=1
 car_images = glob.glob('./vehicles/**/*.png')
@@ -36,25 +31,42 @@ if debug_prt:
     print(notcars[0])
 
 ### Tweak these parameters and see how the results change.
-colorspace = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+color_space = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
 orient = 9
 pix_per_cell = 8
-cell_per_block = 1
+cell_per_block = 2
 hog_channel = "ALL" # Can be 0, 1, 2, or "ALL"
+spatial_size = (32, 32)  # Spatial binning dimensions
+hist_bins = 32  # Number of histogram bins
+spatial_feat = True  # Spatial features on or off
+hist_feat = True  # Histogram features on or off
+hog_feat = True  # HOG features on or off
 
 # pickle the feature parameters
 feature_params={'color_space': "RGB", 'orient': orient, 'pix_per_cell': pix_per_cell,
-            'cell_per_block': cell_per_block, 'hog_channel': "ALL"}
-pickle.dump(feature_params, open('feature_params.p', 'wb'))
+                'cell_per_block': cell_per_block, 'hog_channel': "ALL", 'spatial_size':spatial_size,
+                'hist_bins': hist_bins, 'spatial_feat':spatial_feat, 'hist_feat': hist_feat,
+                'hog_feat': hog_feat}
+
+pickle.dump(feature_params, open('feature_params2.p', 'wb'))
 # end of pickling
 
 t=time.time()
-car_features = extract_features(cars, color_space=colorspace, orient=orient,
-                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                        hog_channel=hog_channel)
-notcar_features = extract_features(notcars, color_space=colorspace, orient=orient,
-                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                        hog_channel=hog_channel)
+
+car_features = extract_features(cars, color_space=color_space,
+                                spatial_size=spatial_size, hist_bins=hist_bins,
+                                orient=orient, pix_per_cell=pix_per_cell,
+                                cell_per_block=cell_per_block,
+                                hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                hist_feat=hist_feat, hog_feat=hog_feat)
+
+notcar_features = extract_features(notcars, color_space=color_space,
+                                   spatial_size=spatial_size, hist_bins=hist_bins,
+                                   orient=orient, pix_per_cell=pix_per_cell,
+                                   cell_per_block=cell_per_block,
+                                   hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                   hist_feat=hist_feat, hog_feat=hog_feat)
+
 t2 = time.time()
 print(round(t2-t, 2), 'Seconds to extract HOG features...')
 # Create an array stack of feature vectors
@@ -66,7 +78,6 @@ scaled_X = X_scaler.transform(X)
 
 # Define the labels vector
 y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
-
 
 # Split up data into randomized training and test sets
 rand_state = np.random.randint(0, 100)
@@ -92,13 +103,16 @@ print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
 print('For these',n_predict, 'labels: ', y_test[0:n_predict])
 t2 = time.time()
 print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
-#pickle
-
-joblib.dump(svc, 'svc_model.p')
+#pickle the model
+joblib.dump(svc, 'svc_model2.p')
+joblib.dump(X_scaler, 'X_scaler_model2.p')
 #pickle.dump(svc, open('svc_model.p', 'wb'))
 
-svc2=joblib.load('svc_model.p')
-
+svc2=joblib.load('svc_model2.p')
+# technically this is done
+#
+#
+#to see if the model is file is working
 print('Test Accuracy of loaded SVC = ', round(svc2.score(X_test, y_test), 4))
 # Check the prediction time for a single sample
 t=time.time()
@@ -107,4 +121,3 @@ print('My SVC load predicts: ', svc2.predict(X_test[0:n_predict]))
 print('For these',n_predict, 'labels: ', y_test[0:n_predict])
 t2 = time.time()
 print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
-#pickle
